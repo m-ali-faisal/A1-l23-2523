6ec5be5fc71bb59fe376fae2a0b9b1fd085837b7
ali <mealifaisal@gmail.com>
2026-09-08 07:01:43 +0500

    preprocessin branch

--- src/train_2523.py
+++ src/train_2523.py
@@ -6,7 +6,7 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
-
+from sklearn.preprocessing import MinMaxScaler
 
 data = pd.read_csv("data/flight_dataset.csv")
 
@@ -21,6 +21,10 @@ data = pd.get_dummies(
 
 
 X = data.drop("Price", axis=1)
+
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
 y = data["Price"]
 
 
